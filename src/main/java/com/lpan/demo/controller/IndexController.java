786d10f3b466d5f45358e439bb8e5147b305eec0
anyu <lipan13022527387@aliyun.com>
2019-11-03 08:16:56 +0800

    界面

---
+++ src/main/java/com/lpan/demo/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.lpan.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sys")
+public class IndexController {
+	
+	@GetMapping("/index")
+	public String index() {
+		return "html/index";
+	}
+
+}
